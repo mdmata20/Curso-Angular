fbd4f190db86c38657d4b8ba6f76331f611f90f4
Manuel De mata <manuel.demata@aicsacorp.com>
2023-06-29 09:20:05 -0600

    IF and for

--- my-store/src/app/app.component.ts
+++ my-store/src/app/app.component.ts
@@ -1,10 +1,67 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  
+  persona = {
+    Nombre: "",
+    edad: 0 
+  }
+  
   title = 'my-store';
+
+  nombres: string[] = ["Manuel", "Josue", "Carlos", "Mario"];
+  newname = '';
+
+  products = [
+    {
+      name: 'EL mejor juguete',
+      price: 565,
+      image: './assets/images/toy.jpg',
+      category: 'all',
+    },
+    {
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: './assets/images/bike.jpg'
+    },
+    {
+      name: 'Colleción de albumnes',
+      price: 34,
+      image: './assets/images/album.jpg'
+    },
+    {
+      name: 'Mis libros',
+      price: 23,
+      image: './assets/images/books.jpg'
+    },
+    {
+      name: 'Casa para perro',
+      price: 34,
+      image: './assets/images/house.jpg'
+    },
+    {
+      name: 'Gafas',
+      price: 3434,
+      image: './assets/images/glasses.jpg'
+    }
+  ]
+
+  Usuario() {
+    this.persona.Nombre = "Manuel";
+  }
+
+  AgregarNombres(){
+    this.nombres.push(this.newname);
+    this.newname = "";
+  }
+
+  deleteUser(indice: number){
+    this.nombres.splice(indice, 1);
+  }
 }
